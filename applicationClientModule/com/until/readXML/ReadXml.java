5396f54cabce5dcd9ff4d68ed6ad63ff50b36817
Menger <1120994538@qq.com>
2016-02-14 08:53:09 +0800

    ff

--- applicationClientModule/com/until/readXML/ReadXml.java
+++ applicationClientModule/com/until/readXML/ReadXml.java
@@ -2,22 +2,26 @@ package com.until.readXML;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import com.alibaba.fastjson.JSON;
+import com.pjo.web.FilterRecord;
+import com.pjo.web.ListenerRecord;
 import com.pjo.web.ServletRecord;
 
 public class ReadXml {
 
-	private static String path="D:\\Users\\wb-limeng.g\\workspaceNews\\zest\\WebContent\\WEB-INF\\web.xml";
-	
+	private static String path = "D:\\yytgk\\ol_java\\WebContent\\WEB-INF\\web.xml";
+
 	public static void read() {
 		File f = new File(path);
 		DocumentBuilder db = null;
@@ -26,30 +30,85 @@ public class ReadXml {
 			dbf = DocumentBuilderFactory.newInstance();
 			db = dbf.newDocumentBuilder();
 			Document doc = db.parse(f);
-			
-			getServletList(doc);
+
+			System.out.println(JSON.toJSON(getServletList(doc)));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public static List<ServletRecord> getServletList(Document doc){
-		List<ServletRecord> servletList=new ArrayList<ServletRecord>();
-		NodeList nodes=doc.getElementsByTagName("servlet-mapping");
+
+	public static Map<String, String> getMap(Document doc,String []a){
+		NodeList nodes = doc.getElementsByTagName(a[0]);
+		Map<String, String> servlet = new HashMap<String, String>();
 		for (int i = 0; i < nodes.getLength(); i++) {
-			Node node=nodes.item(i);
-			NodeList nodeChildren=node.getChildNodes();
+			Node node = nodes.item(i);
+			NodeList nodeChildren = node.getChildNodes();
+			String key="";
+			String value="";
 			for (int j = 0; j < nodeChildren.getLength(); j++) {
-				Node nodeChild=nodeChildren.item(j);
-				System.out.println(nodeChild.getNodeName());
-				System.out.println(nodeChild.getTextContent());
+				Node nodeChild = nodeChildren.item(j);
+				if (!"#text".equals(nodeChild.getNodeName())) {
+					if (a[1].equals(nodeChild.getNodeName())) {
+						key=nodeChild.getTextContent();
+					}else if (a[2].equals(nodeChild.getNodeName())) {
+						value=nodeChild.getTextContent();
+					}
+				}
 			}
+			servlet.put(key,value);
 		}
-		
-		return servletList;
+		return servlet;
 	}
 	
+	public static List<ServletRecord> getServletList(Document doc) {
+		List<ServletRecord> servletList = new ArrayList<ServletRecord>();
+		Map<String, String> servletPath=getMap(doc,new String[]{"servlet","servlet-name","servlet-class"});
+		Map<String, String> servlet=getMap(doc,new String[]{"servlet-mapping","servlet-name","url-pattern"});
+		
+		for (String key : servletPath.keySet()) {
+			ServletRecord srd = new ServletRecord();
+			srd.setServletClass(servletPath.get(key));
+			srd.setServletName(key);
+			srd.setUrlpattern(servlet.get(key));
+			servletList.add(srd);
+		}
+		return servletList;
+	}
+
+	public static List<FilterRecord> getFilterList(Document doc){
+		List<FilterRecord> filterList = new ArrayList<FilterRecord>();
+		Map<String, String> filter=getMap(doc,new String[]{"filter","filter-name","filter-class"});
+		Map<String, String> filterMap=getMap(doc,new String[]{"filter-mapping","filter-name","url-pattern"});
+		
+		for (String key : filter.keySet()) {
+			FilterRecord srd = new FilterRecord();
+			srd.setFilterClass(filter.get(key));
+			srd.setFilterName(key);
+			srd.setUrlpattern(filterMap.get(key));
+			filterList.add(srd);
+		}
+		return filterList;
+	}
+	
+	public static List<ListenerRecord> getListenerList(Document doc){
+		List<ListenerRecord> listenerList = new ArrayList<ListenerRecord>();
+		NodeList nodes = doc.getElementsByTagName("listener");
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			NodeList nodeChildren = node.getChildNodes();
+			for (int j = 0; j < nodeChildren.getLength(); j++) {
+				Node nodeChild = nodeChildren.item(j);
+				if (!"#text".equals(nodeChild.getNodeName())) {
+					if ("listener-class".equals(nodeChild.getNodeName())) {
+						ListenerRecord lrd=new ListenerRecord();
+						lrd.setListenerClass(nodeChild.getTextContent());
+						listenerList.add(lrd);
+					}
+				}
+			}
+		}
+		return listenerList;
+	}
 	public static void main(String[] args) {
 		read();
 	}
